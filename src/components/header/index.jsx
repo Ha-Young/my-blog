c51e0c1e514bdec6e076e3051c8f934b897ddff2
ha-young <hayeong28@naver.com>
2020-11-23 16:57:37 +0900

    add: logo Image in header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,31 +1,60 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, StaticQuery } from 'gatsby'
 import { GitHubIcon } from '../social-share/github-icon'
+import Image from 'gatsby-image'
 
 import './index.scss'
 
-export const Header = ({ title, location, rootPath }) => {
-  const isRoot = location.pathname === rootPath
-  return (
-    <div className="header">
-      {!isRoot && (
-        <Link to={`/`} className="link">
-          {title}
-        </Link>
-      )}
-      <div className="header-navigator">
-        <Link className="nav-item" to="/blog">
-          Blog
-        </Link>
-        <Link className="nav-item" to="/category">
-          Category
-        </Link>
-        <Link className="nav-item" to="https://ha-young.github.io/resume_web/">
-          Resume
-        </Link>
-      </div>
+export const Header = ({ title, location, rootPath }) => (
+  <StaticQuery
+    query={headerQuery}
+    render={data => {
+      const isRoot = location.pathname === rootPath
+      const { fixed: logoImg } = data.logoImg.childImageSharp
+      console.log('img', logoImg)
 
-      <GitHubIcon />
-    </div>
-  )
-}
+      return (
+        <div className="header">
+          <div className="header-home">
+            {!isRoot && (
+              <Link to={`/`} className="link">
+                <Image
+                  className="logo-image"
+                  fixed={logoImg}
+                  alt="logo-image"
+                  imgStyle={{
+                    borderRadius: `50%`,
+                  }}
+                />
+              </Link>
+            )}
+          </div>
+          <div className="header-navigator">
+            <Link className="nav-item" to="/blog">
+              Blog
+            </Link>
+            <Link className="nav-item" to="/category">
+              Category
+            </Link>
+            <Link className="nav-item" to="/resume_web">
+              Resume
+            </Link>
+            <GitHubIcon />
+          </div>
+        </div>
+      )
+    }}
+  />
+)
+
+const headerQuery = graphql`
+  query headerQuery {
+    logoImg: file(absolutePath: { regex: "/logo-haong.png/" }) {
+      childImageSharp {
+        fixed(width: 80, height: 80) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
