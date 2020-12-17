d951cf7e58b6eccd0885405e0eabb44914aef506
Ha0 <hayeong28@naver.com>
2020-12-17 23:51:34 +0900

    add: Tags component

---
+++ src/components/tags/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Item } from './item'
+
+import './index.scss'
+
+export const Tags = ({ tags }) => {
+  console.log(tags)
+  return (
+    <ul className="tags">
+      {tags.map((tag, idx) => (
+        <Item
+          key={idx}
+          tag={tag}
+          onClick={() => {
+            console.log(tag, 'clicked')
+          }}
+        />
+      ))}
+    </ul>
+  )
+}
