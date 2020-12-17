a185af52743c79d835075b661311dae6a847d614
Ha0 <hayeong28@naver.com>
2020-12-17 23:51:57 +0900

    add: apply Tags component on ThumbnailItem

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import { Tags } from '../tags'
 import { Link } from 'gatsby'
 import { TARGET_CLASS } from '../../utils/visible'
 
 import './index.scss'
 
 export const ThumbnailItem = ({ node }) => (
-  <>
-    <span>{node.frontmatter.tags}</span>
-    <Link className={`thumbnail ${TARGET_CLASS}`} to={node.fields.slug}>
-      <div key={node.fields.slug}>
-        <h3>{node.frontmatter.title || node.fields.slug}</h3>
+  <div className={`thumbnail ${TARGET_CLASS}`}>
+    <div key={node.fields.slug}>
+      <Link to={node.fields.slug}>
+        <h3>{node.frontmatter.title || node.fields.slug}</h3>{' '}
+      </Link>
+
+      {node.frontmatter.tags && <Tags tags={node.frontmatter.tags} />}
+
+      <Link to={node.fields.slug}>
         <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-      </div>
-    </Link>
-  </>
+      </Link>
+    </div>
+  </div>
 )
