c4377b99ca0bbea007c7d8491cde416d3aa1fb48
Ha0 <hayeong28@naver.com>
2020-12-24 01:16:43 +0900

    add: get tags in gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,15 +1,18 @@
 const path = require(`path`)
+const _ = require('lodash')
+
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
   const blogPostTemplate = path.resolve(`./src/templates/blog-post.js`)
+  const tagTemplate = path.resolve('src/templates/tags.js')
 
   return graphql(
     `
       {
-        allMarkdownRemark(
+        postsRemark: allMarkdownRemark(
           sort: { fields: [frontmatter___date], order: DESC }
           limit: 1000
         ) {
@@ -26,6 +29,11 @@ exports.createPages = ({ graphql, actions }) => {
             }
           }
         }
+        tagsGroup: allMarkdownRemark(limit: 2000) {
+          group(field: frontmatter___tags) {
+            fieldValue
+          }
+        }
       }
     `
   ).then(result => {
@@ -34,7 +42,7 @@ exports.createPages = ({ graphql, actions }) => {
     }
 
     // Create blog posts pages.
-    const posts = result.data.allMarkdownRemark.edges.filter(
+    const posts = result.data.postsRemark.edges.filter(
       ({ node }) => !node.frontmatter.draft && !!node.frontmatter.category
     )
 
@@ -49,6 +57,19 @@ exports.createPages = ({ graphql, actions }) => {
           slug: post.node.fields.slug,
           previous,
           next,
+        },
+      })
+    })
+
+    // Create tags pages
+    const tags = result.data.tagsGroup.group
+
+    tags.forEach(tag => {
+      createPage({
+        path: `/tags/${_.kebabCase(tag.fieldValue)}/`,
+        component: tagTemplate,
+        context: {
+          tag: tag.fieldValue,
         },
       })
     })
