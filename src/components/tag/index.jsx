cb3ff7ec9a782414de3e2c8c9b2908dd7de9f260
Ha0 <hayeong28@naver.com>
2020-12-25 05:07:52 +0900

    add: tag component link to post with tag page

--- src/components/tag/index.jsx
+++ src/components/tag/index.jsx
@@ -1,11 +1,15 @@
+import { Link } from 'gatsby'
 import React from 'react'
+import kebabCase from 'lodash/kebabCase'
 
 import './index.scss'
 
 export const Tag = ({ tag, onClick }) => {
   return (
     <li className="tag">
-      <div onClick={onClick}>{tag}</div>
+      <Link to={`/tags/${kebabCase(tag)}/`}>
+        <div onClick={onClick}>{tag}</div>
+      </Link>
     </li>
   )
 }
