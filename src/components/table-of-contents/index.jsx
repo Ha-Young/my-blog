e237bd648e094a38256ff227024e590529dfb204
ha-young <hayeong28@naver.com>
2020-12-31 18:33:20 +0900

    fix: TOC styling

--- src/components/table-of-contents/index.jsx
+++ src/components/table-of-contents/index.jsx
@@ -4,6 +4,9 @@ import * as Dom from '../../utils/dom'
 
 import './index.scss'
 
+// TOC 읽은 주제 표시
+// @copyright https://codepen.io/Radiergummi/pen/bjEZMO
+
 export const TableOfContents = ({ toc }) => {
   const onClickTOCOpen = e => {
     const tocContent = e.target.previousSibling
@@ -12,12 +15,14 @@ export const TableOfContents = ({ toc }) => {
     }
   }
   return (
-    <div className="toc-wrapper">
-      <div
-        className="toc-content"
-        dangerouslySetInnerHTML={{ __html: toc }}
-      ></div>
-      <div className="toc-open-btn" onClick={onClickTOCOpen}></div>
+    <div className="toc-container">
+      <div className="toc-wrapper">
+        <div
+          className="toc-content"
+          dangerouslySetInnerHTML={{ __html: toc }}
+        ></div>
+        <div className="toc-open-btn" onClick={onClickTOCOpen}></div>
+      </div>
     </div>
   )
 }
