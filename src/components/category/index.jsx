d6fd0713bf2288ac300e17efc9b660d6c924a828
ha-young <hayeong28@naver.com>
2020-12-31 14:49:56 +0900

    fix: mobile display none effect when  category extend on scrolling

--- src/components/category/index.jsx
+++ src/components/category/index.jsx
@@ -3,46 +3,58 @@ import { rhythm } from '../../utils/typography'
 import './index.scss'
 import { Item } from './item'
 
-const CATEGORY_CONTAINER_CLASSNAME = "category-container"
-const CATEGORY_WRAPPER = "category-wrapper"
+const CATEGORY_CONTAINER_CLASSNAME = 'category-container'
+const CATEGORY_WRAPPER = 'category-wrapper'
+const CATEGORY_EXTEND_LIMIT = 725
 
 export const Category = ({ categories, category, selectCategory }) => {
   const containerRef = useRef(null)
   const wrapperRef = useRef(null)
   let observer = useRef(null)
 
-  const scrollToCenter = useCallback(tabRef => {
-    const { offsetWidth: tabWidth } = tabRef.current
-    const { scrollLeft, offsetWidth: containerWidth } = containerRef.current
-    const tabLeft = tabRef.current.getBoundingClientRect().left
-    const containerLeft = containerRef.current.getBoundingClientRect().left
-    const refineLeft = tabLeft - containerLeft
-    const targetScollX = scrollLeft + refineLeft - (containerWidth / 2) + (tabWidth / 2)
+  const scrollToCenter = useCallback(
+    tabRef => {
+      const { offsetWidth: tabWidth } = tabRef.current
+      const { scrollLeft, offsetWidth: containerWidth } = containerRef.current
+      const tabLeft = tabRef.current.getBoundingClientRect().left
+      const containerLeft = containerRef.current.getBoundingClientRect().left
+      const refineLeft = tabLeft - containerLeft
+      const targetScollX =
+        scrollLeft + refineLeft - containerWidth / 2 + tabWidth / 2
 
-    containerRef.current.scroll({ left: targetScollX, top: 0, behavior: 'smooth' })
-  }, [containerRef])
+      containerRef.current.scroll({
+        left: targetScollX,
+        top: 0,
+        behavior: 'smooth',
+      })
+    },
+    [containerRef]
+  )
 
   useEffect(() => {
     observer = new IntersectionObserver(
       ([e]) => {
         e.target.classList.toggle('isTop', e.intersectionRatio < 1)
 
-        if (e.target.classList.contains('isTop')) {
-          e.target.children[0] && (e.target.children[0].style.width = null)
+        if (
+          e.target.classList.contains('isTop') &&
+          e.target.clientWidth >= CATEGORY_EXTEND_LIMIT
+        ) {
+          e.target.children[0] && (e.target.children[0].style.width = '100%')
         } else {
-          e.target.children[0] && (e.target.children[0].style.width = rhythm(26))
+          e.target.children[0] &&
+            (e.target.children[0].style.width = rhythm(26))
         }
-
       },
       { threshold: [1] }
-    );
+    )
 
     observer.observe(wrapperRef.current)
 
-    return function () {
+    return function() {
       observer.unobserve(wrapperRef.current)
-    };
-  }, []);
+    }
+  }, [])
 
   return (
     <div className={CATEGORY_WRAPPER} ref={wrapperRef}>
@@ -53,10 +65,15 @@ export const Category = ({ categories, category, selectCategory }) => {
         id="category"
         style={{
           margin: `0 -${rhythm(3 / 4)}`,
-          width: rhythm(26)
+          width: rhythm(26),
         }}
       >
-        <Item title={'All'} selectedCategory={category} onClick={selectCategory} scrollToCenter={scrollToCenter} />
+        <Item
+          title={'All'}
+          selectedCategory={category}
+          onClick={selectCategory}
+          scrollToCenter={scrollToCenter}
+        />
         {categories.map((title, idx) => (
           <Item
             key={idx}
